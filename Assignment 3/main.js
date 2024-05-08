792e7616278fd632b75e195dc5022712e0aef0a0
Adeena Imran <adeenaimran01@gmail.com>
2024-05-08 21:24:56 +0500

    fisrt commit

---
+++ Assignment 3/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// storing the famous quote and its author
+let quote = "'Pain is inevitable. Suffering is optional";
+let author = " Haruki Murakami";
+// printing the message
+console.log(`"${quote}" - ${author}`);
---
+++ Assignment 3/main.ts
@@ -0,0 +1,9 @@
+// storing the famous quote and its author
+
+let quote:string="'Pain is inevitable. Suffering is optional"
+
+let author:string= " Haruki Murakami"
+
+// printing the message
+
+console.log(`"${quote}" - ${author}`);
